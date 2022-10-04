1b22898906b69e5dbe38dedd7d32745bf0e7ccbf
Austin David <austin@austindavid.com>
2022-10-03 23:23:21 -0400

    Update sign.sh
    updated with some goodness; masking the destination with dest.ssh (not checked in)

--- stratus/sign.sh
+++ stratus/sign.sh
@@ -9,9 +9,8 @@
 # <filename> must include ^secret key: <gibberish>\n
 #
 # this will be added (prepended) to the content for MD5 hashing.
-# A new file will be created ($1.signed), then uploaded.  You
-# probably need to edit the last couple lines.
-
+# A new file will be created ($1.signed), then uploaded thusly:
+# scp $file.signed $(cat dest.ssh)/$file
 
 FILENAME=$1
 if [ -z "$FILENAME" -o ! -f "$FILENAME" ]; then
@@ -29,10 +28,20 @@ fi
 SIGNED_FILE=$FILENAME.signed
 egrep -v "^MD5|^secret key:" $FILENAME > $SIGNED_FILE
 
-MD5SUM=$((echo $SECRET; cat $SIGNED_FILE) | md5)
+MD5SUM=$((echo $SECRET; cat $SIGNED_FILE) | md5 | cut -f 1 -d " ")
 echo "MD5SUM: $MD5SUM"
 
-echo "MD5: $MD5SUM" >> $SIGNED_FILE
+# echo "MD5: $MD5SUM" >> $SIGNED_FILE
+echo "signature: $MD5SUM" >> $SIGNED_FILE
 
-scp $SIGNED_FILE www:www/df/$(basename $FILENAME)
-aws s3 cp $SIGNED_FILE s3://stratus-iot/$(basename $FILENAME) --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+if [ ! -f dest.ssh ]; then
+  echo "ERROR: dest.ssh must exist"
+  exit 3
+fi
+
+scp $SIGNED_FILE $(cat dest.ssh)/$(basename $FILENAME)
+
+shift
+if [ ! -z "$1" ]; then
+  exec $0 $*
+fi
